d823ef03c8c2e2f42fe1ee5f0f2c1782ba5b77c0
Rohan Raj <20280562+rajrohan@users.noreply.github.com>
2020-04-20 12:17:59 +0200

    Update organiseDesktop.py

--- organise_desktop/organiseDesktop.py
+++ organise_desktop/organiseDesktop.py
@@ -243,6 +243,7 @@ def undo():
 
     # list of folders to be created
     elif sys.platform == 'linux' or sys.platform == 'darwin':
+        # if environ.get('TEST_DIR') != '':
         if environ.get('TEST_DIR'):
             desk_to_dir = environ.get('TEST_DIR')
             
