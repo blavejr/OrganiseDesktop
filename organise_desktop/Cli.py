30fcba54eae5934b769c98b047f30e1a80815804
arcanearronax <arcanearronax@gmail.com>
2020-07-28 01:42:07 -0400

    Rebuilt the Cli.cli method to rely on Click to process command line arguments. This includes modifying the flags used to call various processes and how file extensions are provided. The to_clean dict in Cli.cli was modified to rely on more pythonic comprehension, rather than for loops. The requirements file was updated to include the Click module as well.

--- organise_desktop/Cli.py
+++ organise_desktop/Cli.py
@@ -4,6 +4,7 @@ from cronController import schedule_start, schedule_end
 import os
 import sys
 import json
+import click
 
 pwd = os.path.dirname(os.path.abspath(__file__))
 
@@ -11,80 +12,72 @@ Extensions = json.load(open(pwd+'/Extension.json'))
 
 folders = [x for x in Extensions]
 
-def print_usage():
+@click.command()
+@click.option(
+    "--undosched",
+    "-u",
+    is_flag=True,
+    help="Undo changes that were made.",
+)
+@click.option(
+    "--sched",
+    "-s",
+    is_flag=True,
+    help="Schedule an organization process to run."
+)
+@click.option(
+    "--desched",
+    "-d",
+    is_flag=True,
+    help="Remove a scheduled organization process."
+)
+@click.option(
+    "--ignore",
+    "-i",
+    type=str,
+    multiple=True,
+    help="Identify which file extension to exclude from organization. This should be passed for each file extension."
+)
+def cli(
+    undosched,
+    sched,
+    desched,
+    ignore
+):
     """
-        Prints usage of the Command Line interface
+    This is used to call a command line interface.
     """
-    print("Usage: " + sys.argv[0] + " <argument>")
-    print("-h                                 -- Display help message.")
-    print("-u                                 -- Undo")
-    print("-c --all                           -- Clean. If given --all then cleans all otherwise prompts")
-    print("-s --r                             -- start a schedule. removes a schedule if --r given")
-
-if __name__ == '__main__':
-    
-    if len(sys.argv) <= 1:
-        print_usage()
-
-    elif sys.argv[1] == '-h':
-        print_usage()
-
-    elif sys.argv[1] == '-u':
+    if undosched:
         undo()
-        sys.exit()
 
-    elif sys.argv[1] == '-c':
-        
-        if len(sys.argv) == 3:
-            
-            if sys.argv[2] == '--all':
-                organise_desktop(Extensions)
-                sys.exit()
-                
-            else:
-                print("Invalid input")
-                sys.exit()    
-            
-        elif len(sys.argv) == 2:
-            for x in enumerate(folders):
-                print('{} - {}'.format(x[0], x[1]))
-                
-            try:    
-                exclude = input("Enter indexes of types to EXCLUDE\nEnter multiple by separating by a ,:\n")
-            except KeyboardInterrupt:
-                sys.exit()
+    elif sched:
+        schedule_start(folders)
 
-            exclude_list = [int(i) for i in exclude.split(',')]
-            to_clean = {}
-            
-            for i in range(len(folders)):
-                if i not in exclude_list:
-                    to_clean[folders[i]] = Extensions[folders[i]]
-                    
-                    
-            organise_desktop(to_clean)
-            
-            sys.exit()
-        
-        else:
-            print("Invalid Input")
-            sys.exit()
+    elif desched:
+        schedule_end()
 
-    elif sys.argv[1] == "-s":
-        
-        if len(sys.argv) == 2:
-            print("Starting schedule..")
-            schedule_start(folders)
-            
-        elif sys.argv[2] == '--r':
-            print("Removing schedule..")
-            schedule_end()
-            
-        else:
-            print("Invalid Input")
-            sys.exit()    
+    elif ignore:
+
+        tmp_ext_list = []
+        for i in ignore: # Create a tuple with all the given extensions
+            tmp_ext_list.extend(i.split(','))
+
+        extension_tuple = tuple([ # add periods to the start of extensions if not already there
+            x if x[0] == '.' else f".{x}" for x in tmp_ext_list
+        ])
+
+        to_clean = { # create a dict that doesn't include the provided extensions, based on the extensions in the json file
+            category: [
+                extension for extension in Extensions[category] if extension not in extension_tuple
+            ] for category in Extensions
+        }
+
+        organise_desktop(to_clean)
 
     else:
-        print("Invalid Input")
-        sys.exit()            
-        
+        organise_desktop(Extensions)
+
+    sys.exit()
+
+if __name__ == '__main__':
+    cli()
