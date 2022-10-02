a086b917d3f54121a13925e3976281fda7240c5a
shashank1623 <shashankbhardwaj2030@gmail.com>
2022-10-02 22:24:15 +0530

    Changes have madein file cronCleanUp.py , extension.json ,requirements.txt and readme.md

--- organise_desktop/cronCleanUp.py
+++ organise_desktop/cronCleanUp.py
@@ -4,11 +4,11 @@ import os
 import sys
 
 separator = ""
-if sys.platform == 'win32':
+if sys.platform == 'win64':
     separator = '\\'
 else:
     separator = '/'
-with open(os.path.dirname(os.path.join(os.path.abspath(__file__), 'settings.txt'), 'rb') as setting_file:
+with open(os.path.dirname(os.path.join(os.path.abspath(__file__), 'settings.txt'), 'rb')) as setting_file:
     folders = pickle.load(setting_file)
 
 Clean.main(folders)
